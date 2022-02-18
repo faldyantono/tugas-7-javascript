7eb9f3942656ed824446ac683762bc67000484ab
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-19 02:02:59 +0700

    Add files via upload

---
+++ tugas7.js
@@ -0,0 +1,7 @@
+tinggibadan = [165, 166, 157, 152, 179, 174, 164, 156, 163, 168];
+for (var i = 0; i < tinggibadan.length; i++) {
+  console.log(tinggibadan[i]);
+}
+for (let x of tinggibadan) {
+  console.log(x);
+}
